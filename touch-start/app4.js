2db011e001f66b47136d1de754a55ad78c626cee
Jerichothomason <66220108+Jerichothomason@users.noreply.github.com>
2020-09-12 20:14:09 +0530

    Add files via upload

---
+++ touch-start/app4.js
@@ -0,0 +1,38 @@
+const navbar=document.querySelector(".navbar");
+navbar.classList.add('show');
+const ll=document.querySelector(".last");
+
+var prev=ll.getBoundingClientRect().top;
+var now;
+var down=false;
+var up=false;
+window.addEventListener('scroll', floater);
+window.addEventListener('touchstart', floater);
+document.body.addEventListener('touchstart', floater);
+document.body.addEventListener('scroll', floater);
+
+    function floater(){
+        now=ll.getBoundingClientRect().top;
+        console.log("working");
+        if(now<prev&&!down)
+            //go down
+            {
+                navbar.classList.toggle('hide-nav');
+                console.log("down");
+                // alert('down');
+                down=true;
+                up=false;
+            }
+    
+    
+            if(now>prev&&!up)
+            //go down
+            {
+                navbar.classList.toggle('hide-nav');
+                console.log("up");
+                down=false;
+                up=true;
+            }
+    
+            prev=now;
+    }
