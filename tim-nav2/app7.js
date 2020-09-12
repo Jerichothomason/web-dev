6fab0bd0c9ac3834b76f968a8b6727a4e1d5637c
Jerichothomason <66220108+Jerichothomason@users.noreply.github.com>
2020-09-12 23:30:58 +0530

    Add files via upload

---
+++ tim-nav2/app7.js
@@ -0,0 +1,37 @@
+const ll=document.querySelector(".last");
+const navbar=document.querySelector(".navbar");
+navbar.classList.add("show");
+setInterval(floater, 50);
+
+var down=false;
+var up=false;
+var prev=ll.getBoundingClientRect().top;
+var now;
+
+function floater(){
+    console.log("workinggg"+ll.getBoundingClientRect().top);
+    now=ll.getBoundingClientRect().top;
+    // console.log("working");
+    if(now<prev&&!down)
+        
+        {
+            // go down
+            navbar.classList.toggle('hide-nav');
+            console.log("down");
+            // alert('down');
+            down=true;
+            up=false;
+        }
+
+
+        if(now>prev&&!up)
+        //go down
+        {
+            navbar.classList.toggle('hide-nav');
+            console.log("up");
+            down=false;
+            up=true;
+        }
+
+        prev=now;
+}
